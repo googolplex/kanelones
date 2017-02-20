3d23484bc62f962e835d9fb4a23c115d073b75d1
Roger Armoa <googolplex@lcompras.biz>
2017-02-20 09:32:22 -0300

    comenzando el projecto

--- gitfeliz/grabar_enviar_limones.sh
+++ gitfeliz/grabar_enviar_limones.sh
@@ -7,9 +7,9 @@
 cp /home/xoldfusion/Descargas/openxava-5.6_kanelones/tomcat/conf/server.xml /home/xoldfusion/Descargas/kanelones2017/tomcatfeliz
 cp /home/xoldfusion/Descargas/openxava-5.6_kanelones/tomcat/conf/config.xml /home/xoldfusion/Descargas/kanelones2017/tomcatfeliz
 cp /home/xoldfusion/Descargas/openxava-5.6_kanelones/tomcat/conf/web.xml /home/xoldfusion/Descargas/kanelones2017/tomcatfeliz
-cp /tomcatfeliz/server.xml  kanelones/tomcatfeliz/
-cp /tomcatfeliz/context.xml  kanelones/tomcatfeliz/
-cp /tomcatfeliz/web.xml  kanelones/tomcatfeliz/
+cp tomcatfeliz/server.xml  kanelones/tomcatfeliz/
+cp tomcatfeliz/context.xml  kanelones/tomcatfeliz/
+cp tomcatfeliz/web.xml  kanelones/tomcatfeliz/
 cp *.sh kanelones/gitfeliz
 cp -r openshiftfeliz kanelones/
 cp -r jasperfeliz kanelones/
